227b55861bb7e168ad7f24478105f3c79f8dec13
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-06-03 00:04:32 +0300

    Initial commit - Solve easy array/hashing problems in the neetcode 150 set

---
+++ neetcode-150/01-array-and-hashing/242-valid-anagram.py
@@ -0,0 +1,38 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+    1 <= s.length, t.length <= 5 * 104
+    s and t consist of lowercase English letters.
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count_s, count_t = {}, {}
+
+        for i in range(len(s)):
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
+
+        return count_s == count_t
+
+
+# test
+print(Solution().isAnagram("anagram", "nagaram"))
+print(Solution().isAnagram("rat", "car"))
+print(Solution().isAnagram("", ""))
+print(Solution().isAnagram("a", "a"))
